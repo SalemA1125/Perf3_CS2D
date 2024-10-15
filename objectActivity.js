a065f6485da04f71a26990d24efbd37f09400917
SalemA1125 <andreavaldezsalem@gmail.com>
2024-10-15 19:40:51 +0800

    objectActivity.js

---
+++ objectActivity.js
@@ -0,0 +1,24 @@
+// Define an object named 'car' with properties: type, model, and color
+let car = {
+  type: "Honda",
+  model: "Civic",
+  color: "Red",
+};
+
+// Log the type of the 'car' variable, which will display 'object'
+console.log(typeof car);
+
+// Output the entire 'car' object, showing its current properties
+console.log(car);
+
+// Change the 'type' property of the 'car' object to "Toyota"
+car.type = "Toyota";
+
+// Output the updated 'car' object to see the change in the 'type' property
+console.log(car);
+
+// Add a new property 'wheels' to the 'car' object and set it to 4
+car.wheels = 4;
+
+// Output the 'car' object to see the new 'wheels' property included
+console.log(car);
